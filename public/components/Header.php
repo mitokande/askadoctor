929f172d3b49cdefaf50c180c6f34abb9c0a40fe
mitokande <benmithat18@gmail.com>
2022-08-06 22:10:05 +0300

    header login links

--- public/components/Header.php
+++ public/components/Header.php
@@ -24,8 +24,8 @@
                         <nav class="col-lg-9 col-6">
                             <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a>
                             <ul id="top_access">
-                                <li><a href="login.html"><i class="pe-7s-user"></i></a></li>
-                                <li><a href="register-doctor.html"><i class="pe-7s-add-user"></i></a></li>
+                                <li><a href="login"><i class="pe-7s-user"></i></a></li>
+                                <li><a href="register"><i class="pe-7s-add-user"></i></a></li>
                             </ul>
                             <div class="main-menu">
                                 <ul>
