1fb00b6ac91896c06c5b91a0195fe9ed9f851a1e
mitokande <benmithat18@gmail.com>
2022-08-09 13:07:56 +0300

    changed search in list doctor page

---
+++ resources/views/livewire/doctorfilter.blade.php
@@ -0,0 +1,12 @@
+<div>
+        {{-- <div>
+        <div>
+            <p>Filters</p>
+        </div>
+        <div>
+            Search: <input type="text" name="search" wire:model.debounce.350ms="search" id="">
+        </div>
+</div> --}}
+
+
+</div>
